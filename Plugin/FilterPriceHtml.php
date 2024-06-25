d49ea5e5a7ff8d581cce52c92e1e72ab9af6d2cd
alexsolonenko <oleksii.solonenko@leanswift.com>
2024-06-25 07:57:26 +0300

    CPH-200 - remove PHP 7 support - compatibility with new CustomerPrice module version

--- Plugin/FilterPriceHtml.php
+++ Plugin/FilterPriceHtml.php
@@ -6,19 +6,9 @@ namespace Commerce365\CustomerPriceHyva\Plugin;
 
 use Commerce365\CustomerPrice\Service\PriceInfoProvider\PriceHtmlProvider;
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\View\Design\Theme\ThemeProviderInterface;
-use Magento\Store\Model\StoreManagerInterface;
-use Magento\Theme\Model\Theme\StoreThemesResolver;
 
 class FilterPriceHtml
 {
-    public function __construct(
-        private readonly StoreManagerInterface $storeManager,
-        private readonly StoreThemesResolver $storeThemesResolver,
-        private readonly ThemeProviderInterface $themeProvider
-    ) {}
-
     /**
      * @param PriceHtmlProvider $subject
      * @param callable $proceed
@@ -26,7 +16,6 @@ class FilterPriceHtml
      * @param null $mainProductId
      * @param string $mainProductType
      * @return string
-     * @throws NoSuchEntityException
      */
     public function aroundGet(
         PriceHtmlProvider $subject,
@@ -35,14 +24,6 @@ class FilterPriceHtml
         $mainProductId = null,
         $mainProductType = ''
     ): string {
-        $store = $this->storeManager->getStore();
-        $themes = $this->storeThemesResolver->getThemes($store);
-        $themeId = array_shift($themes);
-        $theme = $this->themeProvider->getThemeById($themeId);
-        if (!strpos($theme->getCode(), 'hyva')) {
-            return $proceed($product, $mainProductId, $mainProductType);
-        }
-
         if (!$mainProductId || $product->getId() !== $mainProductId) {
             return $proceed($product, $mainProductId, $mainProductType);
         }
